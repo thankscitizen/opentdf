22dac435af251068f633e2fc4690f864617c16ab
Daniel Ricaud <dricaud@virtru.com>
2022-06-16 16:39:44 -0500

    Adds success and error messages to the python boostrap script associa… (#77)
    * Adds success and error messages to the python boostrap script associated with the secure-remote-storage app
    
    * Removed unused json dep

--- examples/secure-remote-storage/bootstrap.py
+++ examples/secure-remote-storage/bootstrap.py
@@ -12,7 +12,6 @@ KC_ADMIN_PASS = 'mykeycloakpassword'
 REALM = 'tdf'
 USER_REALM = 'master'
 
-
 keycloak_admin = KeycloakAdmin(
   server_url=KC_URL,
   username=KC_ADMIN_USER,
@@ -38,20 +37,26 @@ new_client = keycloak_admin.update_client(keycloak_client_id,
   }
 )
 
-keycloak_admin.add_mapper_to_client(keycloak_client_id,
-  payload={
-    "protocol": "openid-connect",
-    "config": {
-      "id.token.claim": "true",
-      "access.token.claim": "true",
-      "userinfo.token.claim": "false",
-      "remote.parameters.username": "true",
-      "remote.parameters.clientid": "true",
-      "client.publickey": "X-VirtruPubKey",
-      "claim.name": "tdf_claims",
-      "claim.type": "Virtru OIDC to Entity Claim Mapper"
+try:
+  keycloak_admin.add_mapper_to_client(keycloak_client_id,
+    payload={
+      "protocol": "openid-connect",
+      "config": {
+        "id.token.claim": "true",
+        "access.token.claim": "true",
+        "userinfo.token.claim": "false",
+        "remote.parameters.username": "true",
+        "remote.parameters.clientid": "true",
+        "client.publickey": "X-VirtruPubKey",
+        "claim.name": "tdf_claims",
+        "claim.type": "Virtru OIDC to Entity Claim Mapper"
+      },
+      "name": "tdf_claims",
+      "protocolMapper": "virtru-oidc-protocolmapper",
     },
-    "name": "tdf_claims",
-    "protocolMapper": "virtru-oidc-protocolmapper",
-  },
-)
+  )
+
+  print('Protocol mapper successfully created.')
+
+except Exception as e:
+  print('Protocol mapper already exists with same name, bootstrap script skipped.')
