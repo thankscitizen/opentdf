f40aad58168c1d4be74ee4f17471883b772bde03
Elizabeth Healy <35498075+elizabethhealy@users.noreply.github.com>
2023-01-30 10:38:16 -0500

    PLAT-2263 Extend traffic metric gathering (#199)
    * does getting traffic work
    
    * add back checkout, disable build for testing
    
    * try using matrix and writing to file
    
    * matrix.repo
    
    * callback error for write file
    
    * try await write file
    
    * try adding a callback
    
    * seperate name and events for matrix
    
    * fail if odd week
    
    * fail if even week
    
    * if even fail
    
    * try to upload to gc bucket
    
    * try different naming
    
    * fully set up, run full test
    
    * remove testing workflow config
    
    * use env var date - test
    
    * remove test configs
    
    * remove unused job and param

--- scripts/metrics.js
+++ scripts/metrics.js
@@ -1,25 +1,46 @@
-async function getMetrics(github, context) {   
-    //Research
+async function getMetrics(github) {   
+    // Views -- last 14 days
     const views = await github.rest.repos.getViews({
-        owner: context.repo.owner,
-        repo: context.repo.repo,
+        owner: process.env.OWNER,
+        repo: process.env.REPO,
+        })
+    
+    // Clones -- last 14 days
+    const clones = await github.rest.repos.getClones({
+        owner: process.env.OWNER,
+        repo: process.env.REPO,
         })
 
-    //Activation
-    const clones = await github.rest.repos.getClones({
-        owner: context.repo.owner,
-        repo: context.repo.repo,
-        })
-    const forks = await github.rest.repos.listForks({
-    owner: context.repo.owner,
-    repo: context.repo.repo,
+    // General info -- includes forks, stars, watchers
+    const get_info = await github.rest.repos.get({
+        owner: process.env.OWNER,
+        repo: process.env.REPO,
     })
+
+
+    // Commits -- last 14 days getParticipationStats: ["GET /repos/{owner}/{repo}/stats/participation"] -- add togethor last two
+    const commits = await github.rest.repos.getParticipationStats({
+        owner: process.env.OWNER,
+        repo: process.env.REPO,
+        })
+    
+
+    // Referral Sources -- last 14 days getTopReferrers: ["GET /repos/{owner}/{repo}/traffic/popular/referrers"]
+    const referrals = await github.rest.repos.getTopReferrers({
+        owner: process.env.OWNER,
+        repo: process.env.REPO,
+        })
+    
     const data = {
         timestamp: new Date(),
-        event_type: "opentdf-github",
+        event_type: process.env.EVENT_TYPE,
         views: views.data.count, 
         clones: clones.data.count, 
-        forks: forks.data.length
+        forks: get_info.data.forks_count,
+        stars: get_info.data.stargazers_count,
+        watchers: get_info.data.subscribers_count,
+        commits: commits.data.all[commits.data.all.length-1] + commits.data.all[commits.data.all.length-2],
+        top_referral_sources: referrals.data
     }
     // Retained Usage: Opening Issues, opening PRs, writing comments, posting/commenting on Discussions
     return data
