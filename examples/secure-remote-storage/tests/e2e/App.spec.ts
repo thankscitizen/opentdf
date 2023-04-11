6a6cfd9d4bcea026f40035e762da7b4fd26275de
sievdokymov-virtru <100794336+sievdokymov-virtru@users.noreply.github.com>
2023-04-11 18:02:45 +0300

    Add hosts to /etc/hosts for Secure Storage integration setup  (#233)

--- examples/secure-remote-storage/tests/e2e/App.spec.ts
+++ examples/secure-remote-storage/tests/e2e/App.spec.ts
@@ -34,6 +34,7 @@ test.describe('<App/>', () => {
     await expect(page.locator(selectors.pageTitle)).toBeVisible();
   });
 
+  // test works properly locally but fails on CI for now (PLAT-2424)
   test.skip('able to perform file Encrypt/Upload and then Download/Decrypt, able to replace target file', async ({ page }) => {
     const originalFile = 'fileforupload.docx'
     const replacementFile = 'fileForReplacement.jpeg'
